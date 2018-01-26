991521409e44afd3bfc8882f038fab4d67a86ca5
Jeremy Holman <jeremy.holman@gmail.com>
2018-01-26 05:52:52 -0800

    03 - silly metaprogramming for config POJSO

--- 03_tetr/src/frame/Game.js
+++ 03_tetr/src/frame/Game.js
@@ -1,7 +1,6 @@
 import autobind from 'autobind-decorator';
 
-
-// maybe move to graphics lib
+// maybe move to a graphics lib?
 function get2dContext(canvas_id){
   var canvas = document.getElementById(canvas_id);
   if (!canvas){
@@ -16,39 +15,86 @@ function get2dContext(canvas_id){
   }
 }
 
+function deepObjectAssign(target, varArgs) {
+  // This function adapted from the shim at
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+  assert(typeof target === 'object' && target !== null);
+  for (var i = 1; i < arguments.length; i++) {
+    var nextSource = arguments[i];
+    if (nextSource != null) { // Skip over if undefined or null
+      for (var nextKey in nextSource) {
+        // Avoid bugs when hasOwnProperty is shadowed
+        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+          if (typeof nextSource[nextKey] === 'object' && nextSource[nextKey] !== null) {
+            // Hilarious constructor-invokation to get prototype and properties all fixed up.
+            // I dunno what this does on things other than Arrays, but maybe it works?
+            if (typeof target[nextKey] == 'object' && 
+                target[nextKey] !== null && 
+                Object.getPrototypeOf(target[nextKey]) === Object.getPrototypeOf(nextSource[nextKey])   ) {
+              var subtarget = target[nextKey];
+            } else {
+              var subtarget = new (Object.getPrototypeOf(nextSource[nextKey]).constructor)();
+            }
+            target[nextKey] = deepObjectAssign(subtarget, nextSource[nextKey]);
+          } else {
+            target[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+  }
+  return target;
+}
+
+
+const defaultGameConfig = {
+  physics: {
+    tickSize_ms: 10
+  }
+};
+
+
 @autobind
 class Game {
   constructor(config) {
-    this.config = config;
+    this.config = deepObjectAssign({}, defaultGameConfig, config);
+
     this.context = get2dContext(config.targetDiv);
-    this.lastPhysicsTick = Date.now()
     if (!this.context) throw "giving up on being a Game";
+
+    this.bookkeeping = {
+      previousTick_ms: Date.now()
+    }
   }
 
 
-  render() {
-    console.log(this.lastPhysicsTick);
+  _render() {
+    this.physics();
+
+    // how does rendering work in general?  just enumerate renderEntities in z-order or something?
 
     //requestAnimationFrame(this.render);
   }
 
-  physics() {
-    if (!this.lastPhysicsTick) {
-      lastPhysicsTick = Date.now() - physicsTickSize_ms;
-    }
-
+  _physics() {
     var now = Date.now();
-
-
-    while ((now - lastPhysicsTick) > physicsTickSize_ms) {
+    if (!this.bookkeeping.previousTick_ms) {
+      this.bookkeeping.previousTick_ms = now - this.config.physics.tickSize_ms;
+    }
+    while ((now - this.bookkeeping.previousTick_ms) > this.config.physics.tickSize_ms) {
+      this.physics_tick(this.config.physics.tickSize_ms);
 
       gameState.dirty = true;
-      lastPhysicsTick += physicsTickSize_ms;
+      this.bookkeeping.previousTick_ms += this.config.physics.tickSize_ms;
     }
   }
 
+  physics_tick(tick_size) {
+    // does nothing; override in child class
+  }
+
   start() {
-    requestAnimationFrame(this.render);
+    requestAnimationFrame(this._render);
   }
 }
 
